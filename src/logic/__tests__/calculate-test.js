b8be570ac013ee65d9276b6e9755bb8a01ccba07
Samitti <samuelteweldebrhan@gmail.com>
2021-01-12 11:13:45 +0300

    add unit tests for the calculate.js file

---
+++ src/logic/__tests__/calculate-test.js
@@ -0,0 +1,53 @@
+import calculate from '../calculate';
+
+describe('The calculate component', () => {
+  it('performs addition', () => {
+    const calcData = {
+      total: 2,
+      next: 3,
+      operation: '+',
+    };
+    const btnName = '=';
+    expect(calculate(calcData, btnName)).toStrictEqual({ total: '5', next: null, operation: null });
+  });
+
+  it('performs subtraction', () => {
+    const calcData = {
+      total: 2,
+      next: 3,
+      operation: '-',
+    };
+    const btnName = '=';
+    expect(calculate(calcData, btnName)).toStrictEqual({ total: '-1', next: null, operation: null });
+  });
+
+  it('performs multiplication', () => {
+    const calcData = {
+      total: 2,
+      next: 3,
+      operation: 'x',
+    };
+    const btnName = '=';
+    expect(calculate(calcData, btnName)).toStrictEqual({ total: '6', next: null, operation: null });
+  });
+
+  it('performs division', () => {
+    const calcData = {
+      total: 6,
+      next: 3,
+      operation: '÷',
+    };
+    const btnName = '=';
+    expect(calculate(calcData, btnName)).toStrictEqual({ total: '2', next: null, operation: null });
+  });
+
+  it('performs modulus operation', () => {
+    const calcData = {
+      total: 7,
+      next: 3,
+      operation: '%',
+    };
+    const btnName = '=';
+    expect(calculate(calcData, btnName)).toStrictEqual({ total: '1', next: null, operation: null });
+  });
+});
